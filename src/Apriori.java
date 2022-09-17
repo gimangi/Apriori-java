bb566a3eaefab98927c82c9d168bd648b11ab39a
gimangi <khk211113@naver.com>
2022-09-17 16:12:12 +0900

    project init

---
+++ src/Apriori.java
@@ -0,0 +1,15 @@
+public class Apriori {
+
+    private int sp;
+    private int cf;
+
+    /**
+     *
+     * @param sp : min support
+     * @param cf : confidence
+     */
+    public Apriori(int sp, int cf) {
+        this.sp = sp;
+        this.cf = cf;
+    }
+}
