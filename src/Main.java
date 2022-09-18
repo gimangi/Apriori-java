b15713cad062ce7c822e0d716e2f090148573dab
gimangi <khk211113@naver.com>
2022-09-18 14:17:26 +0900

    input file name

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner sc = new Scanner(System.in);
+    private static String fileName;
     private static final String INPUT_DIR = "input";
     public static void main(String[] args) {
+
+        System.out.print("input file name. \n >> ");
+        fileName = sc.next();
+
         try {
             Apriori apriori = new Apriori(2);
-            TextReader tr = new TextReader(INPUT_DIR + File.separator + "test1.txt");
+            TextReader tr = new TextReader(INPUT_DIR + File.separator + fileName);
             for (List<String> col : tr.getItems()) {
                 apriori.putColumn(col);
             }
